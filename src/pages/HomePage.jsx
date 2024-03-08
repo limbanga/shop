10bf7d565eb34700185655f00a168f309a94b754
limbanga <95570833+limbanga@users.noreply.github.com>
2024-03-08 23:02:09 +0700

    implement sort for price. TODO: implement for time

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,20 +11,59 @@ import React, { useEffect, useState } from "react";
 import ProductBoard from "../components/ProductBoard";
 import ProductFilterList from "../components/ProductFilterList";
 import SortBar from "../components/SortBar";
-import FilterDrawer from "../components/FilterDrawer";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const HomePage = () => {
-  const [openDrawer, setOpenDrawer] = useState(true);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getProducts = () => {
-      const data = [
-        { name: "Product name1", price: 999, image: "/askjdhk.png" },
-        { name: "Product name2", price: 999, image: "/askjdhk.png" },
-        { name: "Product name3", price: 999, image: "/askjdhk.png" },
-        { name: "Product name4", price: 999, image: "/askjdhk.png" },
+      let data = [
+        {
+          name: "Product name1",
+          price: 123,
+          image: "/askjdhk.png",
+          category: "Dress",
+        },
+        {
+          name: "Product name2",
+          price: 345,
+          image: "/askjdhk.png",
+          category: "Shoes",
+        },
+        {
+          name: "Product name3",
+          price: 999,
+          image: "/askjdhk.png",
+          category: "Shoes",
+        },
+        {
+          name: "Product name4",
+          price: 333,
+          image: "/askjdhk.png",
+          category: "Jacket",
+        },
       ];
+
+      const category = searchParams.get("cate");
+      if (category) {
+        data = data.filter((x) => x.category == category);
+      }
+
+      const orderBy = searchParams.get("orderBy");
+      switch (orderBy) {
+        case "low_price":
+          data = data.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+          break;
+        case "high_price":
+          data = data.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+          break;
+          // TODO: Add case for time sort
+        default:
+          break;
+      }
       setProducts(data);
     };
 
@@ -40,7 +79,10 @@ const HomePage = () => {
           </Paper>
         </Grid>
         <Grid item xs={12} md={9}>
-          <SortBar itemFoundCount={products.length} openFilterDrawer={() => setOpenDrawer(true)} />
+          <SortBar
+            itemFoundCount={products.length}
+            openFilterDrawer={() => setOpenDrawer(true)}
+          />
           <ProductBoard products={products} />
         </Grid>
       </Grid>
