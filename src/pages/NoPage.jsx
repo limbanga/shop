7eaaf819bea755f0a22db123ba059c8c408c4a97
limbanga <95570833+limbanga@users.noreply.github.com>
2024-03-10 22:42:59 +0700

    add 404 page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,13 @@
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 
-const NoPage = () => {
-  return <div>NoPage- 404</div>;
-};
-
-export default NoPage;
+export default function NoPage() {
+  return (
+    <Container maxWidth="sm" sx={{textAlign: 'center'}} >
+      <img src="/404.png" alt="404.png" style={{ width: "100%" }} />
+      <Typography variant="h5">There no thing here!!</Typography>
+      <Button LinkComponent={RouterLink} to="/">Back to home page</Button>
+    </Container>
+  );
+}
