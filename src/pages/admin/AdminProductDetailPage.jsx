28e142b0e5298ec5d8defe46cd8a3e74eb485e1b
limbanga <95570833+limbanga@users.noreply.github.com>
2024-04-01 10:38:34 +0700

    change some ui in admin product detail

--- src/pages/admin/AdminProductDetailPage.jsx
+++ src/pages/admin/AdminProductDetailPage.jsx
@@ -11,8 +11,6 @@ import React, { useEffect } from "react";
 import { Link as RouterLink, useParams } from "react-router-dom";
 import { axiosInstance } from "../../api/AxiosInstance";
 import { Add, ArrowBack, Edit } from "@mui/icons-material";
-import { grey } from "@mui/material/colors";
-import { set } from "react-hook-form";
 
 export const AdminProductDetailPage = () => {
   const { id } = useParams();
@@ -71,101 +69,114 @@ export const AdminProductDetailPage = () => {
 
   return (
     <Container sx={{ mt: "5rem" }}>
-      <Button
-        LinkComponent={RouterLink}
-        to="/admin/"
-        color="dark"
-        startIcon={<ArrowBack />}
-      >
-        Back
-      </Button>
-
-      <Typography variant="h3" gutterBottom>
-        Product detail
-      </Typography>
-
       <Box>
-        <Typography variant="h5" gutterBottom>
-          {product?.name}
-        </Typography>
-        <Typography variant="h6" gutterBottom>
-          {product?.code}
-        </Typography>
-        <Typography variant="h6" gutterBottom>
-          {product?.category?.name}
-        </Typography>
-      </Box>
-
-      <Divider />
-
-      <Box mt={".5rem"} display={"flex"} justifyContent={"space-between"}>
-        <Typography variant="h5" gutterBottom>
-          Product Variants
-        </Typography>
         <Button
-          color="info"
-          variant="contained"
-          disableElevation
-          endIcon={<Add />}
+          LinkComponent={RouterLink}
+          to="/admin/"
+          color="dark"
+          startIcon={<ArrowBack />}
         >
-          New variant
+          Back
         </Button>
       </Box>
 
       <Grid container columnSpacing={3} my={".5rem"}>
-        {variants.map((x) => (
-          <Grid key={x.id} item xs={3}>
-            <Paper
-              variant="outlined"
-              square
-              sx={{
-                height: "300px",
-              }}
+        {/* product */}
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h3" gutterBottom>
+            Product detail
+          </Typography>
+
+          <Box>
+            <Typography variant="h5" gutterBottom>
+              {product?.name}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              {product?.code}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              {product?.category?.name}
+            </Typography>
+          </Box>
+        </Grid>
+        {/* variants */}
+        <Grid item xs={12} sm={6}>
+          <Divider sx={{ display: { xs: "block", sm: "none" } }} />
+          {/* information bar */}
+          <Box mt={".5rem"} display={"flex"} justifyContent={"space-between"}>
+            <Typography variant="h5" gutterBottom>
+              Product Variants
+            </Typography>
+            <Button
+              color="info"
+              variant="contained"
+              disableElevation
+              endIcon={<Add />}
             >
-              <Box
-                component={"img"}
-                src={x.image}
-                alt="variant"
-                sx={{
-                  height: "100%",
-                  width: "100%",
-                  objectFit: "contain",
-                  objectPosition: "center",
-                }}
-              />
-            </Paper>
+              New variant
+            </Button>
+          </Box>
+          {/* list variants */}
+          <Grid container columnSpacing={3} my={".5rem"}>
+            {variants.map((x) => (
+              <Grid key={x.id} item xs={3}>
+                <Paper
+                  variant="outlined"
+                  square
+                  sx={{
+                    height: "150px",
+                  }}
+                >
+                  <Box
+                    component={"img"}
+                    src={x.image}
+                    alt="variant"
+                    sx={{
+                      height: "100%",
+                      width: "100%",
+                      objectFit: "contain",
+                      objectPosition: "center",
+                    }}
+                  />
+                </Paper>
+              </Grid>
+            ))}
           </Grid>
-        ))}
+        </Grid>
       </Grid>
+
       <Divider />
+      {/* sizes */}
+      <Box>
+        <Typography variant="h5" my={".5rem"}>
+          Sizes
+        </Typography>
 
-      <Typography variant="h5" my={".5rem"}>
-        Sizes
-      </Typography>
+        <Grid container columnSpacing={3} my={".5rem"}>
+          {sizes?.map((x) => (
+            <Grid key={x.id} item xs={6} sm={4} md={3} lg={2}>
+              <Paper
+                variant="outlined"
+                square
+                sx={{
+                  height: "90px",
+                  p: ".5rem",
+                }}
+              >
+                <Box display={"flex"} justifyContent={"space-between"}>
+                  <Typography variant="h6">Size {x.productSize}</Typography>
+                  <Button size="small">
+                    <Edit />
+                  </Button>
+                </Box>
+                <Typography variant="body1">{x.stock} in Stock</Typography>
+                <Typography variant="body1">{x.price} d</Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
 
-      <Grid container columnSpacing={3} my={".5rem"}>
-        {sizes?.map((x) => (
-          <Grid key={x.id} item xs={6} sm={4} md={3} lg={2}>
-            <Paper
-              variant="outlined"
-              square
-              sx={{
-                height: "90px",
-                p: ".5rem",
-              }}
-            >
-              <Box display={'flex'} justifyContent={'space-between'}>
-                <Typography variant="h6">Size {x.productSize}</Typography>
-                <Button size="small" >
-                  <Edit/>
-                </Button>
-              </Box>
-              <Typography variant="body1">{x.stock} in Stock</Typography>
-              <Typography variant="body1">{x.price} d</Typography>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
       {/* Add footer later */}
       <Box mb={"15rem"} />
     </Container>
